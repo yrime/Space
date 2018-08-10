e8015e9c71f9b12fe8fb102ef6b3883278ccc08f
Felix <rodionovfelix@gmail.com>
2018-08-10 15:25:12 +0300

    new branch update

--- newBranch/TestBranch.java
+++ newBranch/TestBranch.java
@@ -26,22 +26,22 @@ public class TestBranch {
 		
 		corps.get(0).setMass(1000);
 		corps.get(0).setRad(5.0);
-		corps.get(0).setPosition(new Vector(-50.0, 15.0, 0.0));
-		corps.get(0).setVitesse(new Vector(0.5, -0.5, 0.0));
+		corps.get(0).setPosition(new Vector(10.0, 0.0, 0.0));
+		corps.get(0).setVitesse(new Vector(0.0, 6.5, 0.0));
 		
 		corps.get(1).setMass(2000);
 		corps.get(1).setRad(20.0);
-		corps.get(1).setPosition(new Vector(70.0, 30.0, 0.0));
-		corps.get(1).setVitesse(new Vector(-0.1, -0.5, 0.0));
+		corps.get(1).setPosition(new Vector(70.0, 0.0, 0.0));
+		corps.get(1).setVitesse(new Vector(0.0, 3.0, 0.0));
 		
 		corps.get(2).setMass(1500);
 		corps.get(2).setRad(10.0);
-		corps.get(2).setPosition(new Vector(50.0, 25.0, 0.0));
-		corps.get(2).setVitesse(new Vector(-0.25, -0.5, 0.0));
+		corps.get(2).setPosition(new Vector(40.0, 0.0, 0.0));
+		corps.get(2).setVitesse(new Vector(0.0, 2.5, 0.0));
 		
 		corps.get(3).setMass(4000);
 		corps.get(3).setRad(15.0);
-		corps.get(3).setPosition(new Vector(-75.0, 0.0, 0.0));
+		corps.get(3).setPosition(new Vector(-90.0, 0.0, 0.0));
 		corps.get(3).setVitesse(new Vector(0.0, -2.5, 0.0));
 		
 		EtoileSystemable espacesysteme = new EtoileSysteme(new Classique(), 0.200);
